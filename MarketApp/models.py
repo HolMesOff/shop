2d7c926f8b84552a4069e7ef66eb7d7f4bd8fa7b
Nikita <maksimvancman@gmail.com>
2024-09-24 10:59:54 +0600

    Django API 3.1

--- MarketApp/models.py
+++ MarketApp/models.py
@@ -1,20 +1,9 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Название категории")
-    parent = models.ForeignKey(
-        'self',  # ссылка на саму модель Category
-        on_delete=models.CASCADE,  # при удалении родительской категории удаляются все её подкатегории
-        null=True,  # позволяет категориям быть верхнего уровня (без родителя)
-        blank=True,  # позволяет не указывать родителя (для категорий верхнего уровня)
-        related_name='subcategories',  # для доступа к подкатегориям через обратную связь
-        verbose_name="Родительская категория"
-    )
-    photo_url = models.URLField(verbose_name="Ссылка на фото", max_length=255)
-
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='subcategories', on_delete=models.CASCADE)
+    photo_url = models.URLField()
 
     def __str__(self):
         return self.name
--- MarketApp/views.py
+++ MarketApp/views.py
@@ -1,8 +1,14 @@
-from django.http import HttpResponse
-from rest_framework import viewsets, generics
+from rest_framework import generics
 from .models import Category
 from .serializers import CategorySerializer
 
 class CategoryListView(generics.ListAPIView):
-    queryset = Category.objects.filter(parent__isnull=True)
+    queryset = Category.objects.filter(parent__isnull=True)  # Получаем только основные категории
     serializer_class = CategorySerializer
+
+class SubcategoryListView(generics.ListAPIView):
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        parent_id = self.kwargs['parent_id']
+        return Category.objects.filter(parent_id=parent_id)  # Получаем подкатегории для заданной категории
