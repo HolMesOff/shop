108b876081c50b513abf8f19244597381f88806d
Nikita <maksimvancman@gmail.com>
2024-12-31 12:12:20 +0600

    Django API 3.1

--- MarketApp/views.py
+++ MarketApp/views.py
@@ -9,6 +9,7 @@ class CategoryListView(generics.ListAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.filter(parent__isnull=True)
 
+
 class SubcategoryListView(generics.ListAPIView):
     serializer_class = CategorySerializer
 
@@ -16,10 +17,16 @@ class SubcategoryListView(generics.ListAPIView):
         parent_id = self.kwargs['parent_id']
         return Category.objects.filter(parent_id=parent_id)
 
+
 class ProductListView(generics.ListAPIView):
     serializer_class = ProductSerializer
 
     def get_queryset(self):
         parent_id = self.kwargs['parent_id']
-        categories = Category.objects.filter(models.Q(id=parent_id) | models.Q(parent__id=parent_id))
-        return Product.objects.filter(category__in=categories)
+        # Фильтруем категории, в которых родительский элемент соответствует заданному parent_id
+        categories = Category.objects.filter(id=parent_id) | Category.objects.filter(parent__id=parent_id)
+        # Ищем товары, которые принадлежат этим категориям
+        return Product.objects.filter(parent__in=categories)
+
+
+
