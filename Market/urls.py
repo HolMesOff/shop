3881e263a5454b9999527f131095c95a2723fafe
Nikita <maksimvancman@gmail.com>
2024-12-31 13:41:50 +0600

    Django API 3.1

--- Market/urls.py
+++ Market/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib import admin
-from MarketApp.views import CategoryListView, SubcategoryListView, ProductListView
+from MarketApp.views import CategoryListView, SubcategoryListView, ProductListView, ProductSearchView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/categories/', CategoryListView.as_view(), name='category-list'),
     path('api/subcategories/<int:parent_id>/', SubcategoryListView.as_view(), name='subcategory-list'),
     path('api/products/<int:parent_id>/', ProductListView.as_view(), name='product-list'),
+    path('api/products/search/', ProductSearchView.as_view(), name='product-search'),
 ]
--- MarketApp/views.py
+++ MarketApp/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from . import models
 from .models import Category, Product
@@ -27,6 +29,37 @@ class ProductListView(generics.ListAPIView):
         categories = Category.objects.filter(id=parent_id) | Category.objects.filter(parent__id=parent_id)
         # Ищем товары, которые принадлежат этим категориям
         return Product.objects.filter(parent__in=categories)
+
+
+from django.db.models import Q
+import re
+
+class ProductSearchView(APIView):
+    def get(self, request, *args, **kwargs):
+        query = request.query_params.get('query', '').strip()
+
+        if query:
+            # Преобразуем запрос в нижний регистр
+            query_lower = query.lower()
+
+            # Фильтрация товаров с использованием регулярных выражений для игнорирования регистра
+            products = Product.objects.filter(
+                Q(name__iregex=r'{}'.format(re.escape(query_lower))) | Q(name__iregex=r'{}'.format(re.escape(query)))
+            )
+
+            # Сериализация результатов
+            serializer = ProductSerializer(products, many=True)
+            return Response(serializer.data, status=200)
+        else:
+            # Если строка поиска пуста, возвращаем пустой список
+            products = Product.objects.none()
+            serializer = ProductSerializer(products, many=True)
+            return Response(serializer.data, status=200)
+
+
+
+
+
 
 
 
